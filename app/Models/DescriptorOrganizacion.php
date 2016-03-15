c595bec29176db3061dd280c83e0e0197f4df09d
LockonF <zurielovsko@gmail.com>
2016-03-15 03:55:44 -0600

    Cambiado nombre de tabla de Descriptor Organización

--- app/Models/DescriptorOrganizacion.php
+++ app/Models/DescriptorOrganizacion.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class DescriptorOrganizacion extends Model
 {
-    protected $table = 'descriptor_organizacion';
+    protected $table = 'Descriptor_Organizacion';
     protected $fillable = ['id','FechaInicio','FechaTermino','TipoResultado','NumeroRegistro'];
 }
